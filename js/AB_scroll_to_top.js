7f469b3a3506f87edc59b2945b96a4d1d9f3e8d8
Andy Beckmann <abec19@gmail.com>
2021-08-01 19:30:38 -0400

    Update name

--- js/AB_scroll_to_top.js
+++ js/AB_scroll_to_top.js
@@ -1,5 +1,5 @@
 /**
- * AB_scroll_top
+ * AB_scroll_to_top
  * version: 1.0.0
  * 
  * scrollTop() A button that brings user to the top of the page after scrolling
@@ -8,8 +8,8 @@
 function scrollTop() {
 
 	var button = document.createElement('button');
-	button.id = 'scrollTop';
-	button.innerHTML = "<svg version='1.1' id='scrollTopSVG' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 54 54' style='enable-background:new 0 0 54 54;' xml:space='preserve'><path d='M38,0H16C7.2,0,0,7.2,0,16v22c0,8.8,7.2,16,16,16h22c8.8,0,16-7.2,16-16V16C54,7.2,46.8,0,38,0z M43.7,36.9 c-0.8,0.8-1.9,1.3-3,1.3s-2.2-0.4-3-1.3L27,26.2L16.3,36.9c-1.7,1.7-4.4,1.7-6.1,0c-1.7-1.7-1.7-4.4,0-6.1L24,17.1 c1.7-1.7,4.4-1.7,6.1,0l13.7,13.7C45.4,32.5,45.4,35.2,43.7,36.9z'/></svg>";
+	button.id = 'scrollToTop';
+	button.innerHTML = "<svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 54 54' style='enable-background:new 0 0 54 54;' xml:space='preserve'><path d='M38,0H16C7.2,0,0,7.2,0,16v22c0,8.8,7.2,16,16,16h22c8.8,0,16-7.2,16-16V16C54,7.2,46.8,0,38,0z M43.7,36.9 c-0.8,0.8-1.9,1.3-3,1.3s-2.2-0.4-3-1.3L27,26.2L16.3,36.9c-1.7,1.7-4.4,1.7-6.1,0c-1.7-1.7-1.7-4.4,0-6.1L24,17.1 c1.7-1.7,4.4-1.7,6.1,0l13.7,13.7C45.4,32.5,45.4,35.2,43.7,36.9z'/></svg>";
 	document.body.appendChild(button);
 
 	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
